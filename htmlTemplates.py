f382df54c044addf36015784c6f295eeafe0c35a
John Fredy Suárez Pérez <jf.suarez@tec.mx>
2025-07-31 00:25:37 -0600

    Fix: Requirements file

---
+++ htmlTemplates.py
@@ -0,0 +1,21 @@
+bot_template='''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/128/12327/12327353.png">
+    </div>
+    <br> Ariel: </br>
+    <div class="message">{{MSG}}</div>
+    <p>
+ </div>   
+'''
+
+user_template='''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://cdn-icons-png.flaticon.com/128/3597/3597789.png">
+    </div>
+    <br> Tú: </br>
+    <div class="message">{{MSG}}</div>
+    <p>
+ </div>   
+'''

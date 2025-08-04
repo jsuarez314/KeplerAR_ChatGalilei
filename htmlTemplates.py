ada40d88841220d2d1db177ef4db4f4119292281
John Fredy Suárez Pérez <jf.suarez@tec.mx>
2025-08-04 17:23:28 -0600

    Feat: Update Profile Galilei

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -1,7 +1,7 @@
 bot_template='''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://cdn-icons-png.flaticon.com/128/6989/6989363.png">
+        <img src="https://drive.usercontent.google.com/download?id=1utm_TZGZ4NdG9YUykeXyi9LLv4EHI9b2">
     </div>
     <br> Galilei: </br>
     <div class="message">{{MSG}}</div>
